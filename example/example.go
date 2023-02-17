d61fd5eebbf1c76776a0aa77a32f53a42ffcbb7d
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-02-17 17:30:41 +0330

    fix: fixing example problem

--- example/example.go
+++ example/example.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ErfanMomeniii/tts"
 )
 
 func main() {
 	_ = tts.Speak("hello my name is Erfan", tts.EnglishUs, true)
 
-	_ = tts.SaveToFile("hello my friend", tts.EnglishUs, "./example", false)
+	fileName, _ := tts.SaveToFile("hello my friend", tts.EnglishUs, "./example", false)
+	fmt.Println(fileName)
 }
